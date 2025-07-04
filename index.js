8dfb7a105b2935e475818b41038d37f420f5f51f
Katiuska Cabezas <katiuskacabezas@gmail.com>
2025-07-04 10:33:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//SERVIDOR //
+
+import express from "express";  
+const app = express(); 
+
+app.get("/ping", (req, res) => {
+  res.send("/pong"); 
+});
+
+const PORT = 3000; 
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
