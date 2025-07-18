a03f2355c406ef90ec653e85ba7a2608b0a740c4
Katiuska Cabezas <katiuskacabezas@gmail.com>
2025-07-18 11:20:36 -0300

    tocken y vercel,json

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import "dotenv/config";
 import express from "express"; // --> Importar express
+import cors from "cors";  // --> Importar cors 
 const app = express();
-// import cors from "cors";  // --> Importar cors 
 
 // Usar MIDDLEWARE para poder leer el cambio con POST -- va antes de las peticiones
 //Llega la petición el middleware detecta si tiene un cuerpo, lo trasnforma para ser leída
 
-// app.use(cors());   //--> usar cors
+app.use(cors());   //--> usar cors
 
 app.use(express.json());  //--> usar express
 
 import productsRouter from './src/routes/products.router.js'
-
 app.use("/api", productsRouter);
+
+import authRouter from "./src/routes/auth.router.js";
+app.use("/api", authRouter);
 
 // Usar MIDDLEWARE para leer un error -- va al final de todas las peticiones
 //Llega la petición pasa por todas las rutas si no detecta nada arroja mensaje de error
